d8300e8566754af51083b619bbf9f7dd4bedfc2f
Cynthia Kiser <cnk@caltech.edu>
2016-03-28 12:11:10 -0700

    Hmmm passes if I use Django's TestCase and Client; passes if I use DRF's APITestCase and APIClient; What if I move the action to validate and save? Still passes

--- example/tests.py
+++ example/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase, Client
+from rest_framework.test import APITestCase, APIClient
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from .models import Color, FavoriteColor
 
@@ -44,18 +46,19 @@ class ExampleTests(TestCase):
         self.assertListEqual(response.context['choices'], ['3'])
         
 
-class ApiTests(TestCase):
+class ApiTests(APITestCase):
+
+    def create_color(self, color_name):
+        c = Color(name=color_name)
+        c.full_clean()
+        c.save()
+        return c
 
     def setUp(self):
         self.user = User(username='someone')
         self.user.save()
-        self.colors = []
         self.color_names = ['blue', 'green', 'yellow', 'orange', 'red']
-        for color in self.color_names:
-            c = Color(name=color)
-            c.full_clean()
-            c.save()
-            self.colors.append(c)
+        self.colors = [self.create_color(name) for name in self.color_names]
     
     def test_color_list_shows_all_colors(self):
         client = Client()
@@ -73,3 +76,45 @@ class ApiTests(TestCase):
         self.assertEqual(len(response.data), 2)
         self.assertCountEqual(list(response.data[0].keys()), ['url', 'user', 'color'])
         
+    def test_create_one_favorite_color_record_returns_that_record(self):
+        user = reverse('user-detail', kwargs={'pk': self.user.id})
+        color = reverse('color-detail', kwargs={'pk': self.colors[4].id})
+        response = Client().post('/api/favs/', {'user': user, 'color': color})
+        self.assertEqual(response.status_code, 201)
+        self.assertCountEqual(list(response.data.keys()), ['url', 'user', 'color'])
+        
+    def test_create_multiple_favorite_color_records(self):
+        '''This test works fine - the getlist is in the view.'''
+        self.assertEqual(len(FavoriteColor.objects.all()), 0)
+        response = APIClient().post('/api/favs/add_multiple/', {'username': self.user.username,
+                                                                'choice': [self.colors[2].id, self.colors[0].id]})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(len(FavoriteColor.objects.all()), 2)
+
+        
+class ApiWSaveMethodTests(APITestCase):
+
+    def create_color(self, color_name):
+        c = Color(name=color_name)
+        c.full_clean()
+        c.save()
+        return c
+
+    def setUp(self):
+        self.user = User(username='someone', password='password')
+        self.user.save()
+        self.color_names = ['blue', 'green', 'yellow', 'orange', 'red']
+        self.colors = [self.create_color(name) for name in self.color_names]
+        self.client = APIClient(enforce_csrf_checks=True)
+        self.client.login(username=self.user.username, password='password')
+
+        
+    def test_create_multiple_favorite_color_records(self):
+        '''This test hints an endpoint that uses a custom serializer save method.'''
+        self.assertEqual(len(FavoriteColor.objects.all()), 0)
+        response = self.client.post('/api/favs/record_favorites/', data={'username': self.user.username,
+                                                                         'choice': [self.colors[2].id,
+                                                                                    self.colors[0].id]})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(len(FavoriteColor.objects.all()), 2)
+
--- example/views.py
+++ example/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.models import User
-from rest_framework import serializers, viewsets
+from rest_framework import serializers, viewsets, status
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.response import Response
 from .models import Color, FavoriteColor
 
 
@@ -46,11 +48,46 @@ class FavoriteColorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = FavoriteColor
         fields = ('url', 'color', 'user')
+        read_only_fields = ('url', 'color', 'user')
 
+    def validate(self, data):
+        print('In validate. Initial data:')
+        print(self.initial_data)
+        data['user'] = get_object_or_404(User, username=self.initial_data['username'])
+        data['color_ids'] = self.initial_data.getlist('choice')
+        return data
+        
+    def create(self, validated_data):
+        new_records = []
+        for id in validated_data['color_ids']:
+            rec = FavoriteColor(user=validated_data['user'], color_id=id)
+            rec.full_clean()
+            rec.save()
+            new_records.append(rec)
+        return new_records
+            
 # ViewSets define the view behavior.
 class FavoriteColorViewSet(viewsets.ModelViewSet):
     queryset = FavoriteColor.objects.all()
     serializer_class = FavoriteColorSerializer
 
-                                        
+    @list_route(methods=['post', 'put'])
+    def add_multiple(self, request):
+        user = get_object_or_404(User, username=request.data['username'])
+        colors = request.data.getlist('choice')
+        for color in colors:
+            record = FavoriteColor(user=user, color_id=color)
+            record.full_clean()
+            record.save()
+        return Response(status=status.HTTP_201_CREATED)
+
+    @list_route(methods=['post', 'put'])
+    def record_favorites(self, request):
+        print('in record_favorites')
+        serializer = FavoriteColorSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
