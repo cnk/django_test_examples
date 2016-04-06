a1adef7ca549c01be8c16a764f9369d83fbd8d44
Cynthia Kiser <cnk@caltech.edu>
2016-04-06 11:02:02 -0700

    reorder to put the important stuff at the top

--- example/tests/test_csrf_param.py
+++ example/tests/test_csrf_param.py
@@ -6,6 +6,109 @@ from ..models import Color
 from pprint import pprint  # noqa
 
 
+class DjangoTests(TestCase):
+    # ############### helper methods ################
+    def create_color(self, color_name):
+        c = Color(name=color_name)
+        c.full_clean()
+        c.save()
+        return c
+
+    def get_or_create_csrf_token(request):
+        token = request.META.get('CSRF_COOKIE', None)
+        if token is None:
+            token = csrf._get_new_csrf_key()
+            request.META['CSRF_COOKIE'] = token
+        request.META['CSRF_COOKIE_USED'] = True
+        return token
+
+    # ############### tests #####################
+    def test_works_if_we_dont_enforce_csrf_checks(self):
+        client = Client(enforce_csrf_checks=False)
+        response = client.post('/', {'choice': 2})
+        self.assertEqual(response.status_code, 200)
+
+    def test_enforce_csrf_works_with_token_in_cookie(self):
+        '''
+        I can't figure out how to set up a request with a proper CSRF token.
+        I think I need the same token in the posted data AND in either a request
+        header or in the cookies.
+
+        In this test I am trying to set it in the cookie for the authorized session.
+        I do see it in client.cookies - but I am NOT seeing authenication info
+        in the cookie, which makes me doubt the validity of the code. And it doesn't work.
+        '''
+        # first set up some model instances we need
+        user = User.objects.create_user(username='someone', password='password')
+        for name in ['blue', 'green', 'yellow', 'orange', 'red']:
+            self.create_color(name)
+
+        client = Client(enforce_csrf_checks=True)
+        print('before login')
+        print(self.client.cookies)
+        client.login(username=user.username, password='password')
+
+        print('after login')
+        print(self.client.cookies)
+
+        token = csrf._get_new_csrf_key()
+        self.client.cookies['csrftoken'] = token
+        print('after setting token')
+        print(self.client.cookies)
+
+        response = client.post('/', {'username': user.username, 'choice': 2, 'csrfmiddlewaretoken': token})
+        self.assertEqual(response.status_code, 200)
+
+    def test_enforce_csrf_works_with_token_in_header(self):
+        '''
+        I can't figure out how to set up a request with a proper CSRF token.
+        I think I need the same token in the posted data AND in either a request
+        header or in the cookies.
+
+        In this test I am trying to set it in the request headers per the docs:
+        https://docs.djangoproject.com/en/1.8/ref/csrf/#ajax
+        '''
+        # first set up some model instances we need
+        user = User.objects.create_user(username='someone', password='password')
+        for name in ['blue', 'green', 'yellow', 'orange', 'red']:
+            self.create_color(name)
+
+        token = csrf._get_new_csrf_key()
+        # client = Client(enforce_csrf_checks=True, HTTP_X_CSRFTOKEN=token, HTTP_REFERER='http://test.example.com/')
+        client = Client(enforce_csrf_checks=True, HTTP_X_CSRFTOKEN=token)
+        # pprint(client.__dict__)
+        response = client.post('/', {'username': user.username, 'choice': 2, 'csrfmiddlewaretoken': token})
+        # pprint(response.__dict__)
+        self.assertEqual(response.status_code, 200)
+
+    def test_csrf_setup(self):
+        '''
+        In the Django client, the Client doesn't have an attribute for enforce_csrf_checks,
+        it only appears in the handler.
+        '''
+        djangoClient = Client(enforce_csrf_checks=True)  # noqa
+        # print('Client:')
+        # pprint(djangoClient.__dict__)
+        # print('Handler:')
+        # pprint(djangoClient.handler.__dict__)
+
+        # ## output is:
+        #
+        # Client:
+        # {'cookies': <SimpleCookie: >,
+        #  'defaults': {},
+        #  'errors': <_io.BytesIO object at 0x10810af10>,
+        #  'exc_info': None,
+        #  'handler': <django.test.client.ClientHandler object at 0x106f9c588>}
+        # Handler:
+        # {'_exception_middleware': None,
+        #  '_request_middleware': None,
+        #  '_response_middleware': None,
+        #  '_template_response_middleware': None,
+        #  '_view_middleware': None,
+        #  'enforce_csrf_checks': True}
+
+
 class ApiTestCaseTests(APITestCase):
 
     def test_csrf_setup(self):
@@ -68,105 +171,3 @@ class ApiTestCaseTests(APITestCase):
         #  '_template_response_middleware': None,
         #  '_view_middleware': None,
         #  'enforce_csrf_checks': False}
-
-
-class DjangoTests(TestCase):
-
-    def test_csrf_setup(self):
-        '''
-        In the Django client, the Client doesn't have an attribute for enforce_csrf_checks,
-        it only appears in the handler.
-        '''
-        djangoClient = Client(enforce_csrf_checks=True)  # noqa
-        # print('Client:')
-        # pprint(djangoClient.__dict__)
-        # print('Handler:')
-        # pprint(djangoClient.handler.__dict__)
-
-        # ## output is:
-        #
-        # Client:
-        # {'cookies': <SimpleCookie: >,
-        #  'defaults': {},
-        #  'errors': <_io.BytesIO object at 0x10810af10>,
-        #  'exc_info': None,
-        #  'handler': <django.test.client.ClientHandler object at 0x106f9c588>}
-        # Handler:
-        # {'_exception_middleware': None,
-        #  '_request_middleware': None,
-        #  '_response_middleware': None,
-        #  '_template_response_middleware': None,
-        #  '_view_middleware': None,
-        #  'enforce_csrf_checks': True}
-
-    def create_color(self, color_name):
-        c = Color(name=color_name)
-        c.full_clean()
-        c.save()
-        return c
-
-    def get_or_create_csrf_token(request):
-        token = request.META.get('CSRF_COOKIE', None)
-        if token is None:
-            token = csrf._get_new_csrf_key()
-            request.META['CSRF_COOKIE'] = token
-        request.META['CSRF_COOKIE_USED'] = True
-        return token
-
-    def test_works_if_we_dont_enforce_csrf_checks(self):
-        client = Client(enforce_csrf_checks=False)
-        response = client.post('/', {'choice': 2})
-        self.assertEqual(response.status_code, 200)
-
-    def test_enforce_csrf_works_with_token_in_cookie(self):
-        '''
-        I can't figure out how to set up a request with a proper CSRF token.
-        I think I need the same token in the posted data AND in either a request
-        header or in the cookies.
-
-        In this test I am trying to set it in the cookie for the authorized session.
-        I do see it in client.cookies - but I am NOT seeing authenication info
-        in the cookie, which makes me doubt the validity of the code. And it doesn't work.
-        '''
-        # first set up some model instances we need
-        user = User.objects.create_user(username='someone', password='password')
-        for name in ['blue', 'green', 'yellow', 'orange', 'red']:
-            self.create_color(name)
-
-        client = Client(enforce_csrf_checks=True)
-        print('before login')
-        print(self.client.cookies)
-        client.login(username=user.username, password='password')
-
-        print('after login')
-        print(self.client.cookies)
-
-        token = csrf._get_new_csrf_key()
-        self.client.cookies['csrftoken'] = token
-        print('after setting token')
-        print(self.client.cookies)
-
-        response = client.post('/', {'username': user.username, 'choice': 2, 'csrfmiddlewaretoken': token})
-        self.assertEqual(response.status_code, 200)
-
-    def test_enforce_csrf_works_with_token_in_header(self):
-        '''
-        I can't figure out how to set up a request with a proper CSRF token.
-        I think I need the same token in the posted data AND in either a request
-        header or in the cookies.
-
-        In this test I am trying to set it in the request headers per the docs:
-        https://docs.djangoproject.com/en/1.8/ref/csrf/#ajax
-        '''
-        # first set up some model instances we need
-        user = User.objects.create_user(username='someone', password='password')
-        for name in ['blue', 'green', 'yellow', 'orange', 'red']:
-            self.create_color(name)
-
-        token = csrf._get_new_csrf_key()
-        # client = Client(enforce_csrf_checks=True, HTTP_X_CSRFTOKEN=token, HTTP_REFERER='http://test.example.com/')
-        client = Client(enforce_csrf_checks=True, HTTP_X_CSRFTOKEN=token)
-        # pprint(client.__dict__)
-        response = client.post('/', {'username': user.username, 'choice': 2, 'csrfmiddlewaretoken': token})
-        # pprint(response.__dict__)
-        self.assertEqual(response.status_code, 200)
